7b708a31cbff58b8a7044080a1b20f97adec6278
matheus <matheuscorocher@gmail.com>
2024-03-02 15:05:49 +0000

    feat: add list by category

--- service/routes.py
+++ service/routes.py
@@ -20,7 +20,7 @@ Product Store Service with UI
 """
 from flask import jsonify, request, abort
 from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
+from service.models import Product, Category
 from service.common import status  # HTTP Status Codes
 from . import app
 
@@ -180,10 +180,15 @@ def list_products():
 
     products = []
     name = request.args.get("name")
+    category = request.args.get("category")
 
     if name:
         app.logger.info("Find by name: %s", name)
         products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
     else:
         app.logger.info("Find All")
         products = Product.all()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -306,6 +306,29 @@ class TestProductRoutes(TestCase):
         for product in found:
             self.assertEqual(product['name'], test_name)
 
+    def test_query_by_category(self):
+        """It should Query Products by category"""
+        products = self._create_products(10)
+        # retrieves the category of the first product in the products list and assigns it to the variable category
+        category = products[0].category
+        # create a list named found, containing products from the products list whose category matches the category variable
+        found = [product for product in products if product.category == category]
+        # check the count of products match the specified category and assign it to the variable found_count
+        found_count = len(found)
+        # Log a debug message indicating the count and details of the products found
+        logging.debug("Found Products [%d] %s", found_count, found)
+        # send an HTTP GET request to the URL specified by the BASE_URL variable, along with a query parameter "category"
+        response = self.client.get(BASE_URL, query_string=f"category={quote_plus(category.name)}")
+        # assert that response status code is 200, indicating a successful request (HTTP 200 OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # retrieve the JSON data from the response
+        data = response.get_json()
+        # assert that the length of the data list (i.e., the number of products returned in the response) is equal to found_count
+        self.assertEqual(len(data), found_count)
+        # use a for loop to check each product in the data list and verify that all returned products belong to the queried category
+        for product in data:
+            self.assertEqual(product['category'], category.name)
+
     ######################################################################
     # Utility functions
     ######################################################################
