7ca1f23833fb70339f2e1d39c51527b7df2fa91d
Matheus <matheuscorocher@gmail.com>
2024-03-02 18:53:30 +0000

    feat: add test checking if even price is a string, it can find all products when find_by_price is called

--- tests/test_models.py
+++ tests/test_models.py
@@ -254,3 +254,15 @@ class TestProductModel(unittest.TestCase):
         # Use a for loop to iterate over the found products and assert that each product's price matches the expected price, to ensure that all the retrieved products have the correct category.
         for product in found:
             self.assertEqual(product.price, price)
+    
+    def test_find_by_price_when_price_is_a_string(self):
+        """It should Find Products by Price even if price is a string"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(str(price))
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
